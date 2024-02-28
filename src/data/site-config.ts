06ec9685b76b0102a3024920b0678393a3df1955
Luiz <cieslakluiz@gmail.com>
2024-02-28 10:49:56 -0300

    formatting Hero, not sure if there's gonna be text + actions yet

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -23,7 +23,7 @@ export type SiteConfig = {
 	image?: Image
 	headerNavLinks?: Link[]
 	socialLinks?: Link[]
-	hero?: Hero
+	hero: Hero
 	postsPerPage?: number
 	projectsPerPage?: number
 }
@@ -56,19 +56,15 @@ const siteConfig: SiteConfig = {
 		},
 	],
 	hero: {
-		title: 'Hi There & Welcome to My Corner of the Web!',
-		text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
-		image: {
-			src: '/hero.jpeg',
-			alt: 'A person sitting at a desk in front of a computer',
-		},
-		actions: [
-			{
-				text: 'Get in Touch',
-				href: '/contact',
-			},
-		],
+		// text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
+		// actions: [
+		// 	{
+		// 		text: 'Get in Touch',
+		// 		href: '/contact',
+		// 	},
+		// ],
 	},
+
 	postsPerPage: 8,
 }
 
