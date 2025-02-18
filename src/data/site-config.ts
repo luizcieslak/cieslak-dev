972641d4e098c37ba53dbcaebcea9e944ef2f5a9
Luiz Cieslak <cieslakluiz@gmail.com>
2025-02-18 09:59:17 -0300

    chore: add bluesky in footer

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -54,6 +54,10 @@ const siteConfig: SiteConfig = {
 			text: 'X/Twitter',
 			href: 'https://twitter.com/_luizcieslak',
 		},
+		{
+			text: 'Bluesky',
+			href: 'https://bsky.app/profile/luizcieslak.bsky.social',
+		},
 	],
 	hero: {
 		// text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
