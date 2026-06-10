c0eef5e2e7bc6339916ba804a345e8a38ef6be09
Luiz Cieslak <cieslakluiz@gmail.com>
2026-06-09 22:09:13 -0300

    fix(seo): trailing slash config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwind from '@astrojs/tailwind'
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://cieslak.dev',
+	trailingSlash: 'always',
 	integrations: [
 		mdx(),
 		sitemap(),
